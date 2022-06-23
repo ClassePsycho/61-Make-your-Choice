89f2446e4064f8f57af0f77093635627cc675ea5
ClassePsycho <107487164+ClassePsycho@users.noreply.github.com>
2022-06-23 09:29:31 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+/*Start Screen*/
+$(".startingbuttons1").click(function() {
+    $(".headfontd").hide();
+    $(".startingbuttons1").hide();
+    $(".startingbuttons2").hide();
+    
+});
+$("blank").click(function() {
+    $("blank").show();
+});
+
+/*End Start Screen*/
+
+/*page 2*/
